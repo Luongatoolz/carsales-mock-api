081fbc4699e552adbd76aaa5d473f61279ef5971
Luong Tran <iamluongtran@gmail.com>
2021-05-17 11:12:12 +1000

    app structure, subscriber api

---
+++ Controllers/SubscriberController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace carsales_mock_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriberController : ControllerBase
+    {
+        private static readonly List<Subscriber> subscribers = new List<Subscriber>{
+            new Subscriber(){
+                Id = 0,
+                Name = "John Doe",
+                Email = "john.doe@gmail.com"
+                }
+        };
+
+        // GET: api/<SubscriberController>
+        [HttpGet]
+        public ActionResult<List<Subscriber>> Get()
+        {
+            try
+            {
+                return subscribers;
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET api/<SubscriberController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Subscriber> GetById(int id)
+        {
+            try
+            {
+                return subscribers.Find(item => item.Id == id);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // POST api/<SubscriberController>
+        [HttpPost]
+        public ActionResult<Subscriber> Post([FromBody] Subscriber value)
+        {
+            try
+            {
+                value.Id = subscribers.Count;
+                subscribers.Add(value);
+                return CreatedAtAction(nameof(GetById), new { id = value.Id }, value);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // DELETE api/<SubscriberController> for testing only
+        [HttpDelete]
+        public void Delete()
+        {
+            subscribers.Clear();
+        }
+    }
+}
---
+++ Subscriber.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace carsales_mock_api
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
